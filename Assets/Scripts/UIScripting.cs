ca12e95ff61358811d64335b281395b76030be7d
Soukupis <joseph.soukup@outlook.com>
2020-10-27 15:08:44 +0100

    finished app

---
+++ Assets/Scripts/UIScripting.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIScripting : MonoBehaviour
+{
+
+    GameObject[] pauseObjects;
+
+    // Use this for initialization
+    void Start()
+    {
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        hidePaused();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                //Debug.Log("Pause",gameObject);
+                Time.timeScale = 0;
+                showPaused();
+            }
+            else
+            {
+                Time.timeScale = 1;
+                hidePaused();
+            }
+        }
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void showPaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    public void hidePaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+}
